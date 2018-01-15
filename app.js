c310bab512a703a142e858097d2801e508eb4c4e
DarisCalinor <candogan.tuzcu@gmail.com>
2018-01-16 01:50:48 +1100

    Make DB connection: working fine. Install mongoose. Edit pages: landing and register.

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 var express     =   require("express"),
     app         =   express(),
-    bodyParser  =   require("body-parser");
+    bodyParser  =   require("body-parser"),
+    mongoose    =   require("mongoose");
     
 //Config
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017", {useMongoClient: true});  //Connect DB
+mongoose.Promise = global.Promise;                                      //Get Mongoose to use the global promise library
+
+//DB Schemas
+var UserSchema    =   new mongoose.Schema({
+    username:   String,
+    password:   String,
+    email:      String
+});
+
+//DB Models
+var User =   mongoose.model("User", UserSchema);
 
 //Initial route
-app.get("/", function(req,res) {
+app.get("/", function(req, res) {
     res.render("landing");
 });
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.get("*", function(req, res) {
+    res.send("No defined route, mate!");
+}); 
 
 //Server starts to listen
 app.listen(3000, function(){
