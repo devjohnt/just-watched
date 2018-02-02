690aa4e35a85b3e5c470bebd3b9a9af7e40d4f6e
DarisCalinor <candogan.tuzcu@gmail.com>
2018-02-02 14:56:43 +1100

    Add Diary Feature > List all movies in diary. Add links to diary and watchlist pages in User Profile page.

--- routes/diary.js
+++ routes/diary.js
@@ -1,65 +1,65 @@
 //  ===  REQUIRES  ======================================================================
 var
-    express = require("express"),
-    router = express.Router(),
-    request = require("request"),
-    db = require("../models/");
-    
+	express = require("express"),
+	router = express.Router(),
+	request = require("request"),
+	db = require("../models/");
+	
 
 //  ===  INDEX - LIST ALL MOVIES IN THE DIARY  ==========================================
 router.get("/", function (req, res) {
-    db.User
-        .findOne({'username': 'candovsky'})
-        .populate('diary.movie')
-        .exec(function (err, user) {
-            // Error handling
-            if (err) {
-                console.log(err);
-            }
-            // Send found user info
-            else {
-                res.send(user);
-            }
-        });
-    // res.render("diary/index");
+	db.User
+		.findOne({'username': 'candovsky'})
+		.populate('diary.movie')
+		.exec(function (err, user) {
+			// Error handling
+			if (err) {
+				console.log(err);
+			}
+			// Send found user info
+			else {
+				res.render("diary/index", {movies: user.diary});
+			}
+		});
+	// res.render("diary/index");
 });
 
 //  ===  NEW - SHOW NEW DIARY ENTRY FORM  ===============================================
 router.get("/new", function(req, res) {
-    var imdbID = req.query.imdbID;
-    var movieId = req.query.movieId;
-    var url = "http://www.omdbapi.com/?apikey=3b7193fb&i=" + imdbID;
-    request(url, function (error, response, body) {
-        if(error) {
-            console.log("Something happened! ", error);
-        } else if(!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
-            res.render("diary/new", { data: data, movieId: movieId });
-        }
-    });
+	var imdbID = req.query.imdbID;
+	var movieId = req.query.movieId;
+	var url = "http://www.omdbapi.com/?apikey=3b7193fb&i=" + imdbID;
+	request(url, function (error, response, body) {
+		if(error) {
+			console.log("Something happened! ", error);
+		} else if(!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render("diary/new", { data: data, movieId: movieId });
+		}
+	});
 });
 
 //  ===  CREATE - ADD NEW DAIRY ENTRY  ==================================================
 router.post("/", function(req, res) {    
-    var diaryEntry = new db.Diary({
-        watchDate: req.body.date,
-        movie: [],
-    });
-    diaryEntry.movie.push(req.body.movieId);
-    db.User.findOne({'username': req.user.username}, function(err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-            user.diary.push(diaryEntry);
-            user.save(function (err, user) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect("/users/:username/diary");
-                }
-            });
-        }        
-    });
+	var diaryEntry = new db.Diary({
+		watchDate: req.body.date,
+		movie: [],
+	});
+	diaryEntry.movie.push(req.body.movieId);
+	db.User.findOne({'username': req.user.username}, function(err, user) {
+		if (err) {
+			console.log(err);
+		} else {
+			user.diary.push(diaryEntry);
+			user.save(function (err, user) {
+				if (err) {
+					console.log(err);
+				} else {
+					res.redirect("/users/:username/diary");
+				}
+			});
+		}        
+	});
 });
 
 //  ===  EXPORTS  =======================================================================
