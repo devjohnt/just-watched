60e7f20deea7a4c0bc446f0817475a072ea03eba
DarisCalinor <candogan.tuzcu@gmail.com>
2018-02-02 19:07:04 +1100

    Search route, more error handling: If search field empty or too many results are turning, user redirects to landing page with flash message.

--- routes/search.js
+++ routes/search.js
@@ -9,11 +9,24 @@ router.get("/", function (req, res) {
     var query = req.query.movieTitle;
     var url = "http://www.omdbapi.com/?apikey=3b7193fb&s=" + query;
     request(url, function (error, response, body) {
+        // Error handling
         if (error) {
             console.log("Something happened! ", error);
-        } else if (!error && response.statusCode == 200) {
+            res.send(error);
+        }
+        // Take data: Error handling or render results
+        else if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
-            res.render("search/results", { data: data });
+            // Error handling
+            if (data.Response === "False") {
+                var errorMessage = data.Error + " Please search again."
+                req.flash("error", errorMessage);
+                res.redirect("/");
+            }
+            // Render results
+            else {
+                res.render("search/results", { data: data });
+            }
         }
     });
 });
